648668cdf2ab278a77c69a8978ddf28a5fac453e
favourOpara <@favouropara48@gmaul.com>
2025-07-07 07:10:02 +0100

    new db

--- backend/abatrades/settings.py
+++ backend/abatrades/settings.py
@@ -90,10 +90,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'abatrades.wsgi.application'
 
 # Database
-DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
-    
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
