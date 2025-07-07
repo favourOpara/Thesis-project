cbfde61d74b541050e855f36dc54fde4e9bee914
favourOpara <@favouropara48@gmaul.com>
2025-07-07 04:14:04 +0100

    file removed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '169.254.37.188',   // <--- use LAN IP here
+    port: 5173,
+    strictPort: true},
   build: {
     sourcemap: false, // This disables source maps for the build
   },
