e5c6891763f49ca4658f7df8c7933218742ca71d
favourOpara <@favouropara48@gmaul.com>
2025-07-10 18:58:30 +0100

    product image

--- backend/products/models.py
+++ backend/products/models.py
@@ -90,7 +90,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='product_images/')
+    image = models.ImageField()
     alt_text = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
