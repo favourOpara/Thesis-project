2488dc1f52770f035066fe9897f5424ec94d6c80
favourOpara <@favouropara48@gmaul.com>
2025-07-10 07:19:23 +0100

    New db

--- backend/abatrades/settings.py
+++ backend/abatrades/settings.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import os
 import dj_database_url
 
+if os.path.exists('.env'):
+    load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
