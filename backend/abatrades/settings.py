f3b87e438df37ca99fc95c51434f141f26d513f3
favourOpara <@favouropara48@gmaul.com>
2025-07-10 05:32:31 +0100

    db sent

--- backend/abatrades/settings.py
+++ backend/abatrades/settings.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv()
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,10 +94,9 @@ WSGI_APPLICATION = 'abatrades.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 # Password validation
