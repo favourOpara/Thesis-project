a2485dd963c10f3c8ba55d9706ffa02c62f7e49f
favourOpara <favouropara48@gmail.com>
2026-04-27 00:35:08 +0100

    fair

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "../components/Footer";
